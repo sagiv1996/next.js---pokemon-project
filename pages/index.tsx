29e43f8b3728a71b952d77b8b979366fb3e4849e
sagiv bonali <sagivb13@gmail.com>
2023-06-06 16:12:13 +0300

    Load data on scrolling

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,21 +1,57 @@
-import type { InferGetStaticPropsType, GetStaticProps } from "next";
+import { List, ListItem } from "@mui/material";
+import { useState, useEffect } from "react";
+import axios from "axios";
+import InfiniteScroll from "react-infinite-scroller";
 
-type Repo = {
-  count: number;
-  next?: string;
-  previous?: string;
+type Result = {
+  name: string;
+  url: string;
 };
 
-export const getStaticProps: GetStaticProps<{
-  repo: Repo;
-}> = async () => {
-  const res = await fetch("https://pokeapi.co/api/v2/pokemon/");
-  const repo = await res.json();
-  return { props: { repo } };
-};
+export default function Index() {
+  const [data, setData] = useState<Result[]>();
+  const [isLoading, setLoading] = useState(false);
+  const [hasMore, setHasMore] = useState(true);
+  const [endpointUrl, setEndpointUrl] = useState(
+    "https://pokeapi.co/api/v2/pokemon"
+  );
 
-export default function Index({
-  repo,
-}: InferGetStaticPropsType<typeof getStaticProps>) {
-  return repo.count;
+  useEffect(() => {
+    getData();
+  }, []);
+
+  const getData = async () => {
+    const { data: response, status } = await axios.get(endpointUrl);
+    if (!response.results || status != 200) return;
+    if (data) {
+      setData([...data, ...response.results]);
+    } else {
+      setData(response.results);
+    }
+    setLoading(false);
+    setEndpointUrl(response.next);
+    if (response.count && data)
+      setHasMore(data ? response.count > data.length : false);
+  };
+
+  if (isLoading) return <p>Loading...</p>;
+  if (!data) return <p>No Loading data data</p>;
+
+  return (
+    <InfiniteScroll
+      loadMore={getData}
+      hasMore={hasMore}
+      loader={
+        <div className="loader" key={0}>
+          Loading ...
+        </div>
+      }
+    >
+      {data?.map((result, index) => (
+        <List key={index}>
+          <ListItem>{result.name}</ListItem>
+        </List>
+      ))}
+    </InfiniteScroll>
+  );
 }
